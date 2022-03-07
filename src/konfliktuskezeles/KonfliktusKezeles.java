a3e9729aa0c0ac6579b789d6135336f8bcbd5bce
kovacsdaniel422 <82216326+kovacsdaniel422@users.noreply.github.com>
2022-03-07 14:48:34 +0100

    Update KonfliktusKezeles.java

--- src/konfliktuskezeles/KonfliktusKezeles.java
+++ src/konfliktuskezeles/KonfliktusKezeles.java
@@ -5,9 +5,9 @@ public class KonfliktusKezeles {
 
     public static void main(String[] args) {
         int i = 7;
-        String nev1= KDCs;
-        String nev2= X.Y;
-        String nev3= Béla;
+        String nev1= "KDCs";
+        String nev2= "X.Y";
+        String nev3= "Béla";
     }
     
 }
